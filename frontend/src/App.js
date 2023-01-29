fb98e61806ef071c2f191c5e1a588e2169ee688f
zishan ahmed <zishan.ahmed1210@gmail.com>
2023-01-30 01:02:41 +0600

    react router v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -26,38 +26,37 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/order/:id' component={OrderScreen} exact />
-          <Route path='/shipping' component={ShippingScreen} exact />
-          <Route path='/payment' component={PaymentScreen} exact />
-          <Route path='/placeorder' component={PlaceOrderScreen} exact />
-          <Route path='/login' component={LoginScreen} exact />
-          <Route path='/register' component={RegisterScreen} exact />
-          <Route path='/profile' component={ProfileScreen} exact />
-          <Route path='/product/:id' component={ProductScreen} exact />
-          <Route path='/cart/:id?' component={CartScreen} exact />
-          <Route path='/admin/userlist' component={UserListScreen} exact />
-          <Route path='/admin/user/:id/edit' component={UserEditScreen} exact />
-          <Route
-            path='/admin/productlist'
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={ProductListScreen}
-            exact
-          />
-          <Route path='/admin/product/:id/edit' component={ProductEditScreen} exact />
-          <Route path='/admin/orderlist' component={OrderListScreen} exact />
-          <Route path='/search/:keyword' component={HomeScreen} exact />
-          <Route path='/:categoryName' component={HomeScreen} exact />
-          <Route path='/page/:pageNumber' component={HomeScreen} exact />
-          <Route
-            path='/search/:keyword/page/:pageNumber'
-            component={HomeScreen}
-            exact
-          />
-          <Route path='/' component={HomeScreen} exact />
+          <Routes>
+            <Route path='/order/:id' element={<OrderScreen />} />
+            <Route path='/shipping' element={<ShippingScreen />} />
+            <Route path='/payment' element={<PaymentScreen />} />
+            <Route path='/placeorder' element={<PlaceOrderScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id?' element={<CartScreen />} />
+            <Route path='/admin/userlist' element={<UserListScreen />} />
+            <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
+            <Route
+              path='/admin/productlist'
+              element={<ProductListScreen />}
+            />
+            <Route
+              path='/admin/productlist/:pageNumber'
+              element={<ProductListScreen />}
+            />
+            <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+            <Route path='/admin/orderlist' element={<OrderListScreen />} />
+            <Route path='/search/:keyword' element={<HomeScreen />} />
+            <Route path='/:categoryName' element={<HomeScreen />} />
+            <Route path='/page/:pageNumber' element={<HomeScreen />} />
+            <Route
+              path='/search/:keyword/page/:pageNumber'
+              element={<HomeScreen />}
+            />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
